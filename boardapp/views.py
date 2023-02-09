8d98eef69eee93be5de8bf2db1b092e9b521e067
佐野泰規 <sanotaiki@sanoyasushikinoMacBook-Pro.local>
2023-02-09 13:50:49 +0900

    updatee

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login,logout
+from .models import BoardModel
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+
 
 def signupfunc(request):
     if request.method =="POST":
@@ -21,7 +27,45 @@ def loginfunc(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return render(request,'login.html',{'context':'logged in'})
+            return redirect('list')
         else:
-            return render(request,'login.html',{'context':'not logged in'})
+            return render(request,'login.html')
     return render(request,'login.html',{'context':'get method'})
+
+
+def listfunc(request):
+    object_list = BoardModel.objects.all()
+    return render(request,'list.html',{'object_list':object_list})
+
+
+def logoutfunc(request):
+    logout(request)
+    return redirect('login')
+
+def detailfunc(request,pk):
+    object = get_object_or_404(BoardModel,pk=pk)
+    return render(request,'detail.html',{'object':object})
+
+def goodfunc(request,pk):
+    object = BoardModel.objects.get(pk=pk)
+    object.good = object.good + 1
+    object.save()
+    return redirect('list')
+
+def readfunc(request,pk):
+    object = BoardModel.objects.get(pk=pk)
+    username = request.user.get_username()
+    if username in object.readtext:
+        return redirect('list')
+    else:
+        object.read = object.read + 1 
+        object.readtext = object.readtext + ' ' + username
+        object.save()
+        return redirect('list') 
+
+
+class BoardCreate(CreateView):
+    template_name= 'create.html'
+    model = BoardModel
+    fields = ('title','content','author','sns_image')
+    success_url = reverse_lazy('list')
